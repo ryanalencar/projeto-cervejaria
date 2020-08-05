d157fc8efa1b00a1d90e71dedda7d75de17126b4
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-08-05 12:06:17 -0300

    test

---
+++ js/contato.js
@@ -0,0 +1,101 @@
+(function ($) {
+  $.fn.floatLabels = function (options) {
+    // Settings
+    var self = this;
+    var settings = $.extend({}, options);
+
+    // Event Handlers
+    function registerEventHandlers() {
+      self.on("input keyup change", "input, textarea", function () {
+        actions.swapLabels(this);
+      });
+    }
+
+    // Actions
+    var actions = {
+      initialize: function () {
+        self.each(function () {
+          var $this = $(this);
+          var $label = $this.children("label");
+          var $field = $this.find("input,textarea").first();
+
+          if ($this.children().first().is("label")) {
+            $this.children().first().remove();
+            $this.append($label);
+          }
+
+          var placeholderText =
+            $field.attr("placeholder") &&
+            $field.attr("placeholder") != $label.text()
+              ? $field.attr("placeholder")
+              : $label.text();
+
+          $label.data("placeholder-text", placeholderText);
+          $label.data("original-text", $label.text());
+
+          if ($field.val() == "") {
+            $field.addClass("empty");
+          }
+        });
+      },
+      swapLabels: function (field) {
+        var $field = $(field);
+        var $label = $(field).siblings("label").first();
+        var isEmpty = Boolean($field.val());
+
+        if (isEmpty) {
+          $field.removeClass("empty");
+          $label.text($label.data("original-text"));
+        } else {
+          $field.addClass("empty");
+          $label.text($label.data("placeholder-text"));
+        }
+      },
+    };
+
+    // Initialization
+    function init() {
+      registerEventHandlers();
+
+      actions.initialize();
+      self.each(function () {
+        actions.swapLabels($(this).find("input,textarea").first());
+      });
+    }
+    init();
+
+    return this;
+  };
+
+  $(function () {
+    $(".float-label-control").floatLabels();
+  });
+})(jQuery);
+
+// Validação
+
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
